a95598d77aa1ab95352b05101c65f95cb3236b4d
José Cervera <joseluis.justinianocervera1a@gmail.com>
2024-11-24 20:49:53 -0600

    Filtrar elementos

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,27 +5,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Filtar
+        //Obtener la cantidad de usuarios con una edad mayor a 18 y su username comience con R.
 
         List<User> users = new ArrayList<>();
 
-        //Obtener la cantidad de usuarios con una edad mayor a 18
         users.add(new User("Rafael", 25));
         users.add(new User("Daniel", 20));
         users.add(new User("Tania", 15));
         users.add(new User("Luisa", 24));
         users.add(new User("Pedro", 21));
 
-        //Imperativo
-        int contador = 0;
-        for (User user : users) {
-            if (user.getAge() > 18) {
-                contador++;
-            }
-        }
-        System.out.println(contador);
-
-        //Declarativo
-        contador = (int)users.stream().filter(user -> user.getAge() > 18).count();
-        System.out.println(contador);
+        long cantidad =  users.stream()//1
+                .filter(user -> user.getAge() > 18)//2
+                .filter(user -> user.getUsername().startsWith("R"))//3
+                .count();//4
+        System.out.println(cantidad);
     }
 }
